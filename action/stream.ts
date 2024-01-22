d15e77cf1763f2799182a0f750e9e8940fecf74f
Aarenrishikdevloper <you@example.com>
2024-01-22 09:22:19 +0530

    redeploy

--- action/stream.ts
+++ action/stream.ts
@@ -1,7 +1,8 @@
 "use server";
-import {Stream} from "@prisma/client";
+
 import {getSelf} from "@/lib/auth-service";
 import {db} from "@/lib/db";
+import { Stream } from "@prisma/client";
 import {revalidatePath} from "next/cache";
 
 export const Updatestream = async(values:Partial<Stream>)=>{
